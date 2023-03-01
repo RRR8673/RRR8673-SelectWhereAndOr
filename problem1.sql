03459ecf91405d46fe2fbc6143520eea0ac620f8
Rafael Renigado <renigado@aol.com>
2023-03-01 19:16:48 +0000

    done

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+SELECT * FROM employee 
+    WHERE first_name LIKE 'Steve'
+    AND salary > 75000;
---
+++ problem3.sql
@@ -0,0 +1,2 @@
+SELECT * FROM employee WHERE salary > 50000 AND 'Steve' NOT IN (first_name);
+-- SELECT * FROM table WHERE value NOT IN ('value1', 'value2');
